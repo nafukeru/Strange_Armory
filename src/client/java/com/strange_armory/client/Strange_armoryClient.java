9ac235890326367011353da6018469e9017469ea
nafukeru <nahukeru@outlook.jp>
2025-01-13 17:23:06 +0900

    ファイル生成

---
+++ src/client/java/com/strange_armory/client/Strange_armoryClient.java
@@ -0,0 +1,10 @@
+package com.strange_armory.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Strange_armoryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/strange_armory/Strange_armory.java
@@ -0,0 +1,10 @@
+package com.strange_armory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Strange_armory implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
