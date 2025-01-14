e4b8696e537c56c922a21d8f6c23beb39a22074e
nafukeru <nahukeru@outlook.jp>
2025-01-14 21:32:24 +0900

    最初のアイテムの追加

--- src/main/java/com/strange_armory/Strange_armory.java
+++ src/main/java/com/strange_armory/Strange_armory.java
@@ -1,10 +1,24 @@
 package com.strange_armory;
 
+import com.strange_armory.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Strange_armory implements ModInitializer {
 
+    //modidを「strange_armory」にする
+    public static final String MOD_ID = "strange_armory";
+
+    //logger(ログファイルの書き込みにいるらしい)をmodidにする
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
+
+        //マイクラがロードされたときに実行される
+        //初期化がうんちゃらカンチャラ
+        LOGGER.info("debug:\"strange armory mod\" successfully loaded");
+        ModItems.registermoditems();
     }
 }
